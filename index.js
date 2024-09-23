f70037f33944b5dfee4d936ec7e714115897b8b6
TylerBarrett-Werx <tbarrett@preswerx.com>
2024-09-23 10:50:15 -0500

    add cocktail.ejs page

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+app.post("/submit", (req, res) => {
+    res.render("cocktail.ejs");
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
