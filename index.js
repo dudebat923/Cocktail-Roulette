3c645bc4ed1c49ac43460f98701c5085f2337f7c
TylerBarrett-Werx <tbarrett@preswerx.com>
2024-09-23 17:21:45 -0500

    Generate a random drink name and matching image

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from 'axios'
 
 const app = express();
 const port = 3000;
@@ -9,10 +10,29 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
+    try {
+        const drink =  await generateCocktail();
+        res.render("cocktail.ejs", {
+            drinkName: drink.strDrink,
+            drinkThumb: drink.strDrinkThumb
+        });
+    } catch (error) {
+        res.render("cocktail.ejs", {drinkName: 'Failed to generate a cocktail'});
+    }
     res.render("cocktail.ejs");
 })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
+
+async function generateCocktail () {
+    try {
+        const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+        return response.data.drinks[0];
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
