cfee895b7f5ffb2575c82b79e1b86cc2b3d39074
TylerBarrett-Werx <tbarrett@preswerx.com>
2024-09-23 20:02:29 -0500

    Generate ingredients and instructions

--- index.js
+++ index.js
@@ -16,8 +16,24 @@ app.post("/submit", async (req, res) => {
     try {
         const drink =  await generateCocktail();
         res.render("cocktail.ejs", {
-            drinkName: drink.strDrink,
-            drinkThumb: drink.strDrinkThumb
+            cocktail: drink,
+            ingredients: [
+                drink.strIngredient1,
+                drink.strIngredient2,
+                drink.strIngredient3,
+                drink.strIngredient4,
+                drink.strIngredient5,
+                drink.strIngredient6,
+                drink.strIngredient7,
+                drink.strIngredient8,
+                drink.strIngredient9,
+                drink.strIngredient10,
+                drink.strIngredient11,
+                drink.strIngredient12,
+                drink.strIngredient13,
+                drink.strIngredient14,
+                drink.strIngredient15,
+            ]
         });
     } catch (error) {
         res.render("cocktail.ejs");
